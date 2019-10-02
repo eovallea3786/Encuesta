ba82f73cbd0276d98478b915c0b9ff6057bd8a91
eovallea3786 <eovallea3786@universidadean.edu.co>
2019-10-01 19:20:22 -0500

    Estructura Cuestionario

---
+++ Cuestionario/Cuestionario.php
@@ -0,0 +1,132 @@
+<?php  
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Entrevista</title>
+	<link rel="stylesheet" type="text/css" href="../css/estilos.css">
+</head>
+<body>
+	<table id="titulo">
+	<tr id="titulo">
+		<td id="imagen">
+			<img src="../file/form.png" id="titulo">
+		</td>
+		<td id="titulo">
+			<p id="titulo">Encuesta empresarial</p>
+		</td>
+	</tr>
+</table>
+	<center>
+		<div>
+			<form>
+			<table>
+					<tr>
+						<td>	
+							<p>Tipo de Identificación:</p>
+						</td>
+						<td>
+							<input type="text" name="Tipo de Identificación" value="Tipo de Identificación">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<p>Número de Identificación:</p>
+						</td>
+						<td>
+							<input type="text" name="Número de Identificación" value="Número de Identificación">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<p>Lugar de Nacimiento:</p>
+						</td>
+						<td>
+							<input type="text" name="Lugar de Nacimiento" value="Lugar de Nacimiento">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<p>Edad:</p>
+						</td>
+						<td>
+							<input type="text" name="Edad" value="Edad">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<p>Sexo:</p>
+						</td>
+						<td>
+							<input type="text" name="Sexo" value="Sexo">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<p>Domicilio:</p>
+						</td>
+						<td>
+							<input type="text" name="Domicilio" value="Domicilio">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<p>Barrio:</p>
+						</td>
+						<td>
+							<input type="text" name="Barrio" value="Barrio">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<p>Teléfono:</p>
+						</td>
+						<td>
+							<input type="text" name="Teléfono" value="Teléfono">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<p>Celular:</p>
+						</td>
+						<td>
+							<input type="text" name="Celular" value="Celular">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<p>Correo electronico:</p>
+						</td>
+						<td>
+							<input type="text" name="Correo electronico" value="Correo electronico">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<p>Nivel educativo:</p>
+						</td>
+						<td>
+							<input type="text" name="Nivel educativo" value="Nivel educativo">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<p>Foto:</p>
+						</td>
+						<td>
+							<input type="file" name="Foto" value="Foto">
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2" align="center">
+							<input type="submit" value="Submit">
+						</td>
+					</tr>
+				</table>
+			</form>
+		</div>
+	<center>
+</body>
+</html>
